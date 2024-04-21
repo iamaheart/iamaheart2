0d3dbc474c2276e826827303bf3b6e2f3e6bfd37
iamaheart <iamahedotart@gmail.com>
2024-04-21 20:30:52 +0900

    240421

--- texts.js
+++ texts.js
@@ -1,1 +1,11 @@
-const texts = ['hello'];
+const _0 = [
+    'Focus on your heart.',
+    '심장으로 부터 온몸의 끝까지 피가 퍼지는 것을 느끼세요',
+    '온몸이 이완되는 것을 느끼세요.',
+    '몸이 붕 뜨는 느낌을 인지하세요.',
+    '저절로 미소가 띄워집니다.',
+]
+
+const _1 = ['Focus on your heart.',]
+
+const texts = [_0,_1,];
