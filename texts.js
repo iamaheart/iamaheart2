a8258a43320f639472f81703ec95979a0b9f6df3
iamaheart <iamahedotart@gmail.com>
2024-05-06 19:45:30 +0900

    commit

--- texts.js
+++ texts.js
@@ -1,11 +1,25 @@
 const _0 = [
-  "Focus on your heart.",
+  "Focus on your own heart.",
   "Blood spreads from the heart to the end of the body.",
   "The whole body relaxes.",
   "I feel like my body is floating.",
   "A smile comes out on its own.",
 ];
 
-const _3 = ["Just rely on yourself."];
+const _3 = [
+  "Just rely on yourself."
+];
+
+const _4 = [
+  "Truth applies to everything."
+];
+
+const _5 = [
+  "Comparison is the root of all evil.",
+];
+
+const _6 = [
+  "Being itself is happiness.",
+];
 
 const texts = [_0, _3];
