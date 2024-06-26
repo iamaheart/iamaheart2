9719fda4a97a0530b966b6ca42c369769ad66c38
iamaheart <iamahedotart@gmail.com>
2024-06-26 21:16:28 +0900

    initial react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a React component inside your existing project.</p>
+    </div>
+  );
+}
+
+export default App;
