502d10d22c16333f89456599598563148d05d4a5
iamaheart <146944830+iamaheart@users.noreply.github.com>
2024-06-27 16:44:29 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
