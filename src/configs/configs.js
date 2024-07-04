72e74de145cf04b8c2305cb0327bbf3f8db34347
iamaheart <146944830+iamaheart@users.noreply.github.com>
2024-07-04 15:51:06 +0900

    Update configs.js

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -1,1 +1,3 @@
-export default URL = "https://iamaheart.github.io/iamaheart/";
+export const URL = "https://iamaheart.github.io/iamaheart/";
+export const DEFAULT_CHARACTER_HEIGHT = 5;
+export const ASCII_CHARACTER_MINUS_CODE = 32;
