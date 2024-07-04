689809226e92a39c07967020277cfe705bf5e4d6
iamaheart <146944830+iamaheart@users.noreply.github.com>
2024-07-04 15:47:18 +0900

    Update and rename App.js to App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import PixelText from "./components/PixelText";
+
+function App() {
+  return (
+    <div>
+      <PixelText text={"i am AB8lFsde352ggdfsGHNM..."} />
+    </div>
+  );
+}
+
+export default App;
