b80aa0bdfa875dfdd35fe0ca6a1597a70310adb5
iamaheart <146944830+iamaheart@users.noreply.github.com>
2024-07-09 16:26:48 +0900

    Update PixelCharacter.tsx

--- src/components/PixelCharacter.tsx
+++ src/components/PixelCharacter.tsx
@@ -1,14 +1,38 @@
-import Character from "./Character";
-import "../styles/PixelCharacter.css";
-import PixelTextProps from "../interfaces/PixelTextProps";
+import { characters } from "../configs/characters";
+import {
+  ASCII_CHARACTER_MINUS_CODE,
+  DEFAULT_CHARACTER_HEIGHT,
+} from "../configs/configs";
+import CharacterProps from "../interfaces/CharacterProps";
 
-export default function PixelCharacter({ text, size, color }: PixelTextProps) {
-  const arr = text.split("");
+export default function PixelCharacter(props: CharacterProps) {
+  let num = props.num;
+  // if (num >= 65 && num <= 90) {
+  //   num = num + 32;
+  // }
+  const arr = characters[num - ASCII_CHARACTER_MINUS_CODE];
+  const x = arr[0].length;
   return (
-    <div className="container" style={{ gap: 10 }}>
-      {arr.map((char: string, index: number) => (
-        <Character num={char.charCodeAt(0)} size={size} key={index} />
-      ))}
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 ${x} ${DEFAULT_CHARACTER_HEIGHT}`}
+      width={x * props.size}
+      height={DEFAULT_CHARACTER_HEIGHT * props.size}
+    >
+      {arr.map((row: number[], rowIndex: number) =>
+        row.map((cell: number, cellIndex: number) =>
+          cell === 1 ? (
+            <rect
+              key={`${rowIndex}-${cellIndex}`}
+              x={cellIndex} // 열(column) 위치
+              y={rowIndex} // 행(row) 위치
+              width="1"
+              height="1"
+              fill="black"
+            />
+          ) : null
+        )
+      )}
+    </svg>
   );
 }
