9e742b643ddc2eb294e5f86f2fdb305ba4e8a8fe
iamaheart <146944830+iamaheart@users.noreply.github.com>
2024-07-10 16:47:17 +0900

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,17 +1,19 @@
-import React from "react";
 import "../styles/MainPage.css";
-import svgImage from "../assets/images/iamahe.art.svg";
-import PixelText from "../components/PixelText";
+import PixelWriting from "../components/PixelWriting";
 
 export default function MainPage() {
   return (
     <div className="App">
-      <img className="center-svg" src={svgImage} alt="SVG Image" />
-      <PixelText text={"!@#$%^&*()_+{}<>?"} size={5} color="#000000" />
-      <PixelText text={"ABCDEFGHIJKLMNOPQRSTUVWXYZ"} size={5} color="#000000" />
-      <PixelText text={"1234567890"} size={5} color="#000000" />
-      <PixelText text={"abcdefghijklmnopqrstuvwxyz"} size={5} color="#000000" />
-      <p>dsfdsfsdfgjfsdjgkjdfsl;</p>
+      {/* <img className="center-svg" src={svgImage} alt="SVG Image" /> */}
+      <div className="main-page">
+        <PixelWriting
+          text={
+            '"I am a boy."\nHello World! I\'m fine thank you, and you?\nBlack pearl.'
+          }
+          size={5}
+          color="#000000"
+        />
+      </div>
     </div>
   );
 }
