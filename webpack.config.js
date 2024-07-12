05055526c20c6e64e6c59f90ec9104e5011b8cb3
iamaheart <146944830+iamaheart@users.noreply.github.com>
2024-07-12 16:41:37 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,16 @@ module.exports = {
         use: "babel-loader",
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.module\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
       },
       {
         test: /\.svg$/,
